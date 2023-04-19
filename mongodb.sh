84e7d047fe2ede53f6a73da6c7798be3966efa11
VenuSCode <58114938+VenuS-Code@users.noreply.github.com>
2023-04-20 02:00:54 +0530

    updated ip address 0.0.0.0 in mongod.conf

--- mongodb.sh
+++ mongodb.sh
@@ -3,4 +3,5 @@ yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 # Need to change default address 127.0.01 to 0.0.0.0 in /etc/mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl restart mongod
