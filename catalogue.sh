064125521add73c6fdc7aa31f92576469c5d4210
VenuSCode <58114938+VenuS-Code@users.noreply.github.com>
2023-04-22 23:59:09 +0530

    adding Paths

--- catalogue.sh
+++ catalogue.sh
@@ -1,18 +1,24 @@
 
 echo -e "\e[32m >>>>>>>>> Configuring the NodeJS repos<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[32m >>>>>>>>> Installing NodeJS <<<<<<<<<\e[0m"
 yum install nodejs -y
+echo -e "\e[32m >>>>>>>>>Adding a Roboshop User <<<<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e[32m >>>>>>>>>Creating a Application Directory <<<<<<<<<\e[0m"
 mkdir /app
+echo -e "\e[32m >>>>>>>>> Downloading code artifacts <<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+echo -e "\e[32m >>>>>>>>> Unzip catatalogue.zip File<<<<<<<<<\e[0m"
 unzip /tmp/catalogue.zip
 cd /app
+echo -e "\e[32m >>>>>>>>> Installing Node Package Manager<<<<<<<<<\e[0m"
 npm install
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp /home/centos/Roboshop/catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
-cp mongo.repo /etc/yum.repos.s/mongo.repo
+cp /home/centos/Roboshop/mongo.repo /etc/yum.repos.s/mongo.repo
 yum install mongodb-org-shell -y
 mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
