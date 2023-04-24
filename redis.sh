b84b1be3cf78e54ceb736ebeaba81e1f5d21f14c
VenuSCode <58114938+VenuS-Code@users.noreply.github.com>
2023-04-25 00:06:27 +0530

    updated everything

--- redis.sh
+++ redis.sh
@@ -5,7 +5,7 @@ dnf module enable redis:remi-6.2 -y
 echo -e "\e[36m >>>>>>> Installing Redis <<<<<\e[0m"
 yum install redis -y
 echo -e "\e[36m >>>>>>> Update the listen address 127.0.0.1 to 0.0.0.0  /etc/redis.conf <<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 # Update the listen address 127.0.0.1 to 0.0.0.0  /etc/redis.conf
 echo -e "\e[36m >>>>>>> Enable redis startup service  <<<<<\e[0m"
 systemctl enable redis
