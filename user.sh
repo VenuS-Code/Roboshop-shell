4c57e0abf935c68e88212d6a981672a7a9d945dd
VenuSCode <58114938+VenuS-Code@users.noreply.github.com>
2023-04-25 00:32:22 +0530

    modified redis

--- user.sh
+++ user.sh
@@ -1,16 +1,25 @@
+echo -e "\e [35m >>>>> Downloading  Node Js repo file <<<<<\e [0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e [35m >>>>> Installing Node Js <<<<<\e [0m"
 yum install nodejs -y
+echo -e "\e [35m >>>>> Adding a user roboshop <<<<<\e [0m"
 useradd roboshop
+echo -e "\e [35m >>>>> Creating a directory <<<<<\e [0m"
 mkdir /app
+echo -e "\e [35m >>>>> Downloading  App code to /app directory <<<<<\e [0m"
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
+echo -e "\e [35m >>>>> unzip the code <<<<<\e [0m"
 unzip /tmp/user.zip
 cd /app
+echo -e "\e [35m >>>>> Installing npm tools <<<<<\e [0m"
 npm install
-cp user.service /etc/systemd/system/user.service
+echo -e "\e [35m >>>>> configure the user.service <<<<<\e [0m"
+cp /home/centos/Roboshop-shell/user.service /etc/systemd/system/user.service
 systemctl daemon-reload
 systemctl enable user
 systemctl start user
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/Roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e [35m >>>>> Installing mongodb <<<<<\e [0m"
 yum install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js
+mongo --host mongodb.devopsbeginner.online </app/schema/user.js
