af7d4d5de507d1b155211d383fadae42fcf014f8
VenuSCode <58114938+VenuS-Code@users.noreply.github.com>
2023-04-25 13:03:43 +0530

    modified the mysql services 1.0

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,10 @@
+echo -e"\e [36m >>>>>>> Enabling mysql required version <<<<<<<\e[0m"
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e"\e [36m >>>>>>> Copying the mysql repo <<<<<<<\e[0m"
+cp /home/centos/Roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e"\e [36m >>>>>>> Installing mysql <<<<<<<\e[0m"
 yum install mysql-community-server -y
+echo -e"\e [36m >>>>>>> Enable startup services and start the mysql servies <<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
 mysql_secure_installation --set-root-pass RoboShop@1
